b9ffc9e2e5cdf4cf32d962db027bc0fbf9efcccf
Jarrod <jarrod83miller@gmail.com>
2021-02-05 19:45:31 -0600

    Fixed spacing issue

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,13 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.get('https://youtube.googleapis.com/youtube/v3/search', {
+    'part': 'snippet',
+    'key': options.key,
+    'maxResults': options.max,
+    'q': options.query,
+    'type': 'video',
+    'videoEmbeddable': true,
+  }, function (data) { callback(data); });
 };
 
 export default searchYouTube;
